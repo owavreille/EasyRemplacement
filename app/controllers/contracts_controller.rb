a31ad1f57a725f0aa1b04af2c4be4a1e12eef9d8
Olivier Wavreille <owavreille@gmail.com>
2023-06-11 17:28:50 +0200

    secure contract controller

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,4 +1,12 @@
 class ContractsController < ApplicationController
+  before_action :require_role
+
+  def require_role
+    unless current_user&.role == true
+      redirect_to root_path, alert: "Accès non autorisé."
+    end
+  end
+  
     def index
       @contract_path = Rails.public_path.join('contrat.html')
       @contract_content = File.read(@contract_path)
--- app/controllers/office_controller.rb
+++ app/controllers/office_controller.rb
@@ -1,5 +1,11 @@
 class OfficeController < ApplicationController
+    before_action :require_active
 
+    def require_active
+      unless current_user&.active == true
+        redirect_to root_path, alert: "Accès non autorisé."
+      end
+    end
 
     def index
         @doctors = Doctor.all
