b527b7e3d3cb58be20f0bd269b866f0b5f545356
Olivier Wavreille <owavreille@gmail.com>
2023-06-18 16:39:41 +0200

    style update

--- app/controllers/shared_controller.rb
+++ app/controllers/shared_controller.rb
@@ -3,7 +3,7 @@ class SharedController < ApplicationController
         @site = Site.find_by(id: params[:site_id])
         @events = @site ? @site.events.where('start_time >= ?', Date.today) : Event.where('start_time >= ?', Date.today)
         @event = Event.all
-      render partial: 'shared/calendar_partial_5_days'
+      render partial: 'shared/calendar_partial_4_days'
     end
   end
   
