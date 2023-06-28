a559c25d9a27e4eeb0f3b2129fa7f1939cf97c6e
Olivier Wavreille <owavreille@gmail.com>
2023-06-28 21:36:10 +0200

    controller error update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,11 @@ class RegistrationsController < Devise::RegistrationsController
     private
   
     def sign_up_params
-      params.require(:user).permit(:title, :last_name, :first_name, :date_of_birth, :phone, :email, :address, :postal_code, :city, :siret_number, :license_number, :password, :password_confirmation, :mailing_list_id)
+      params.require(:user).permit(:title, :last_name, :first_name, :date_of_birth, :phone, :email, :address, :postal_code, :city, :siret_number, :license_number, :password, :password_confirmation, :mailing_list_id, :rib, :speciality)
     end
   
     def account_update_params
-      params.require(:user).permit(:title, :last_name, :first_name, :date_of_birth, :phone, :email, :address, :postal_code, :city, :siret_number, :license_number, :password, :password_confirmation, :current_password, :mailing_list_id)
+      params.require(:user).permit(:title, :last_name, :first_name, :date_of_birth, :phone, :email, :address, :postal_code, :city, :siret_number, :license_number, :password, :password_confirmation, :current_password, :mailing_list_id, :rib, :speciality)
     end
   end
   
