c3fe493dbc2e389c990ed6f6e5a045db8ee5dc98
Olivier Wavreille <owavreille@gmail.com>
2023-07-15 16:35:09 +0200

    sort table and hide sidebar

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.email = auth.info.email
         user.password = Devise.friendly_token[0, 20]
-        user.first_name = auth.info.first_name # assuming the user model has a name
+        user.first_name = auth.info.first_name
         user.last_name = auth.info.last_name
       end
     end
