86e2e38ea148bd4a1745bb82cb892d0b2203a4e5
Olivier Wavreille <owavreille@gmail.com>
2023-07-16 20:12:43 +0200

    fix app settings tests and event show test

--- test/controllers/app_settings_controller_test.rb
+++ test/controllers/app_settings_controller_test.rb
@@ -1,13 +1,24 @@
-require "test_helper"
+require 'test_helper'
 
 class AppSettingsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    @app_settings = app_settings(:one)
+    sign_in @user
+  end
+
   test "should get index" do
-    get app_settings_index_url
+    get app_settings_url
     assert_response :success
   end
 
-  test "should get update" do
-    get app_settings_update_url
-    assert_response :success
+  test "should update app settings" do
+    patch app_setting_url(@app_settings), params: { app_setting: { app_name: "New App Name" } }
+    assert_redirected_to app_settings_path
+    assert_equal "Paramètres mis à jour avec succès !", flash[:notice]
   end
+  
+  
 end
