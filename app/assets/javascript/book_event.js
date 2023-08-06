7b04ebb2ffbb6959746a64ea5c98a9298e07dda7
Olivier Wavreille <owavreille@gmail.com>
2023-08-06 16:22:55 +0200

    fix event booking

--- app/assets/javascript/book_event.js
+++ app/assets/javascript/book_event.js
@@ -2,12 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   const reserveButton = document.querySelector('.reserve-button');
   const form = document.querySelector('.form');
 
-  if (reserveButton && form) {
     reserveButton.addEventListener('click', function(event) {
       event.preventDefault();
       form.submit();
     });
-  } else {
-    console.error('Elements not found');
-  }
 });
