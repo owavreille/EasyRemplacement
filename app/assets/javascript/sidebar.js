71e4bd3b423bcf89f5d7dd043aa34c92a857ace9
Olivier Wavreille <owavreille@gmail.com>
2023-08-06 16:33:13 +0200

    fix sidebar loading and default title option

--- app/assets/javascript/sidebar.js
+++ app/assets/javascript/sidebar.js
@@ -1,33 +1,35 @@
-document.querySelector('.yield-content').addEventListener('click', function() {
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("hidden");
+
+  const container = document.querySelector(".container");
+  container.classList.toggle("sidebar-visible");
+
+  const yieldContent = document.querySelector(".yield-content");
+  yieldContent.classList.toggle("sidebar-visible");
+
+  const navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("sidebar-visible");
+
+  const toggleIcon = document.getElementById("toggleIcon");
+  if (!sidebar.classList.contains("hidden")) {
+    yieldContent.style.marginLeft = "140px";
+    navbar.style.marginLeft = "140px";
+    toggleIcon.classList.remove("bi-list");
+    toggleIcon.classList.add("bi-x");
+  } else {
+    yieldContent.style.marginLeft = "";
+    navbar.style.marginLeft = "";
+    toggleIcon.classList.remove("bi-x");
+    toggleIcon.classList.add("bi-list");
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.yield-content').addEventListener('click', function() {
     const sidebar = document.getElementById('sidebar');
     if (!sidebar.classList.contains('hidden')) {
       toggleSidebar();
     }
   });
-  
-  function toggleSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("hidden");
-  
-    const container = document.querySelector(".container");
-    container.classList.toggle("sidebar-visible");
-  
-    const yieldContent = document.querySelector(".yield-content");
-    yieldContent.classList.toggle("sidebar-visible");
-  
-    const navbar = document.querySelector(".navbar");
-    navbar.classList.toggle("sidebar-visible");
-  
-    const toggleIcon = document.getElementById("toggleIcon");
-    if (!sidebar.classList.contains("hidden")) {
-      yieldContent.style.marginLeft = "140px";
-      navbar.style.marginLeft = "140px";
-      toggleIcon.classList.remove("bi-list");
-      toggleIcon.classList.add("bi-x");
-    } else {
-      yieldContent.style.marginLeft = "";
-      navbar.style.marginLeft = "";
-      toggleIcon.classList.remove("bi-x");
-      toggleIcon.classList.add("bi-list");
-    }
-  }
+});
