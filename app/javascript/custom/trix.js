d846f9ebb23d4c9727e9733652f9c677644177b9
Olivier Wavreille <owavreille@gmail.com>
2023-08-13 16:32:13 +0200

    update underline css

--- app/javascript/custom/trix.js
+++ app/javascript/custom/trix.js
@@ -53,10 +53,10 @@ function toolbarDefaultHTML() {
    <div class="trix-dialogs" data-trix-dialogs>
      <div class="trix-dialog trix-dialog--link" data-trix-dialog="href" data-trix-dialog-attribute="href">
        <div class="trix-dialog__link-fields">
-         <input type="url" name="href" class="trix-input trix-input--dialog" placeholder="${lang.urlPlaceholder}" aria-label="${lang.url}" required data-trix-input>
+         <input type="url" name="href" class="trix-input trix-input--dialog" placeholder="Entrer une URL..." aria-label="${lang.url}" required data-trix-input>
          <div class="trix-button-group">
-           <input type="button" class="trix-button trix-button--dialog" value="${lang.link}" data-trix-method="setAttribute">
-           <input type="button" class="trix-button trix-button--dialog" value="${lang.unlink}" data-trix-method="removeAttribute">
+           <input type="button" class="trix-button trix-button--dialog" value="Lier" data-trix-method="setAttribute">
+           <input type="button" class="trix-button trix-button--dialog" value="Supprimer le Lien" data-trix-method="removeAttribute">
          </div>
        </div>
      </div>
