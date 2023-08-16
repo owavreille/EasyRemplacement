751690d468475e5ec6fb766940b49db074a316b6
Olivier Wavreille <owavreille@gmail.com>
2023-08-16 14:39:33 +0200

    change accounting filters

--- app/controllers/accounting_controller.rb
+++ app/controllers/accounting_controller.rb
@@ -32,44 +32,6 @@ def amounts_by_user
 end
 
 private
-
-def group_and_sum(column_name)
-  group_method = determine_group_method
-  Event.send(group_method, :start_time).sum(column_name)
-end
-
-def group_and_sum_by(entity, column_name)
-  grouped_data = group_events_by(entity, column_name)
-  entity_ids = grouped_data.keys
-  entity_names = entity.to_s.camelize.constantize.where(id: entity_ids).pluck(:first_name, :last_name).map { |first_name, last_name| "#{first_name} #{last_name}" }
-  Hash[entity_names.zip(grouped_data.values)]
-end
-
-def group_events_by(entity, column_name)
-  group_method = case params[:group_by]
-                 when "month"
-                   :group_by_month
-                 when "year"
-                   :group_by_year
-                 else
-                   :group_by_month
-                 end
-
-  Event.group("#{entity}_id").send(group_method, :start_time).sum(column_name)
-end
-  
-def group_and_sum_by_site(column_name)
-  grouped_data = group_events_by_site(column_name)
-  site_ids = grouped_data.keys
-  site_names = Site.where(id: site_ids).pluck(:name)
-  Hash[site_names.zip(grouped_data.values)]
-end
-
-def group_events_by_site(column_name)
-  group_method = determine_group_method
-  Event.group(:site_id).send(group_method, :start_time).sum(column_name)
-end
-
 def determine_group_method
   case params[:group_by]
   when "month"
@@ -80,4 +42,54 @@ def determine_group_method
     :group_by_month
   end
 end
+
+def group_and_sum(column_name)
+  events_filtered_by_year = filter_events_by_month_and_year(Event)
+  group_method = determine_group_method
+  events_filtered_by_year.send(group_method, :start_time).sum(column_name)
+end
+
+def group_and_sum_by(entity, column_name)
+  events_filtered_by_year = filter_events_by_month_and_year(Event)
+  grouped_data = group_events_by(entity, column_name, events_filtered_by_year)
+  entity_ids = grouped_data.keys
+  entity_names = entity.to_s.camelize.constantize.where(id: entity_ids).pluck(:first_name, :last_name).map { |first_name, last_name| "#{first_name} #{last_name}" }
+  Hash[entity_names.zip(grouped_data.values)]
+end
+
+def group_events_by(entity, column_name, events)
+  group_method = determine_group_method
+  events.group("#{entity}_id").send(group_method, :start_time).sum(column_name)
+end
+
+def group_and_sum_by_site(column_name)
+  events_filtered_by_year = filter_events_by_month_and_year(Event)
+  grouped_data = group_events_by_site(column_name, events_filtered_by_year)
+  site_ids = grouped_data.keys
+  site_names = Site.where(id: site_ids).pluck(:name)
+  Hash[site_names.zip(grouped_data.values)]
+end
+
+def group_events_by_site(column_name, events)
+  group_method = determine_group_method
+  events.group(:site_id).send(group_method, :start_time).sum(column_name)
+end
+
+def filter_events_by_month_and_year(events)
+  filtered_events = events
+
+  if params[:year].present?
+    year = params[:year].to_i
+    filtered_events = filtered_events.where(start_time: Date.new(year)..Date.new(year).end_of_year)
+  end
+
+  if params[:month].present?
+    month = params[:month].to_i
+    filtered_events = filtered_events.where(start_time: Date.new(year, month)..Date.new(year, month).end_of_month)
+  end
+
+  filtered_events
+end
+
+
 end
