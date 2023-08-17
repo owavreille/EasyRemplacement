955dad35c35e6575dec43b2e9e3b2e734b238ce1
Olivier Wavreille <owavreille@gmail.com>
2023-08-17 19:01:26 +0200

    improve navbar style

--- app/assets/javascript/sidebar.js
+++ app/assets/javascript/sidebar.js
@@ -5,26 +5,25 @@ function toggleSidebar() {
   const yieldContent = document.querySelector(".yield-content");
   yieldContent.classList.toggle("sidebar-visible");
 
-  const navbar = document.querySelector(".navbar");
-  navbar.classList.toggle("sidebar-visible");
+  const navbarBrand = document.querySelector(".navbar-brand"); // Ciblez le navbar-brand
 
   const toggleIcon = document.getElementById("toggleIcon");
   if (!sidebar.classList.contains("hidden")) {
     yieldContent.style.marginLeft = "140px";
-    navbar.style.marginLeft = "140px";
+    navbarBrand.style.marginLeft = "140px"; // Ajustez le marginLeft du navbar-brand
     toggleIcon.classList.remove("bi-list");
     toggleIcon.classList.add("bi-x");
   } else {
     yieldContent.style.marginLeft = "";
-    navbar.style.marginLeft = "";
+    navbarBrand.style.marginLeft = ""; // Réinitialisez le marginLeft du navbar-brand
     toggleIcon.classList.remove("bi-x");
     toggleIcon.classList.add("bi-list");
   }
 }
 
-  document.querySelector('.yield-content').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-    if (!sidebar.classList.contains('hidden')) {
-      toggleSidebar();
-    }
-  });
+document.querySelector('.yield-content').addEventListener('click', function() {
+  const sidebar = document.getElementById('sidebar');
+  if (!sidebar.classList.contains('hidden')) {
+    toggleSidebar();
+  }
+});
