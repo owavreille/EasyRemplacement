b34b7ce6c89bfd477682e9e4bb23146c207dd9fa
Olivier Wavreille <owavreille@gmail.com>
2023-08-19 14:57:52 +0200

    update for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,18 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+   # Mailer configiration  
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+     address:              ENV['SMTP_ADDRESS'],
+     port:                 587,
+     domain:               ENV['SMTP_DOMAIN'], 
+     user_name:            ENV['SMTP_USER_NAME'], 
+     password:             ENV['SMTP_PASSWORD'], 
+     authentication:       ENV['SMTP_AUTHENTICATION'],
+     enable_starttls_auto: true
+   }
+   
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
