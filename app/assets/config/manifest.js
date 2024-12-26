5343f116d6f668fb316a260193cae684a1e68bff
Olivier Wavreille <owavreille@gmail.com>
2024-12-26 15:29:34 +0100

    Update asset manifest to include application.js and application.css for improved asset management

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,8 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link application.js
+//= link application.css
 //= link sidebar.js
 //= link contract.js
 //= link book_event.js
