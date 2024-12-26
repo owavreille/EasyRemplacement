a376d6eb7786102793879d17aa09bdbd5bd75279
Olivier Wavreille <owavreille@gmail.com>
2024-12-26 16:17:33 +0100

    Change event listener from 'DOMContentLoaded' to 'turbo:load' in book_event.js; added null check for reserveButton and form to ensure proper functionality.

--- app/assets/javascript/book_event.js
+++ app/assets/javascript/book_event.js
@@ -1,9 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('turbo:load', function() {
   const reserveButton = document.querySelector('.reserve-button');
   const form = document.querySelector('.form');
 
+  if (reserveButton && form) {
     reserveButton.addEventListener('click', function(event) {
       event.preventDefault();
       form.submit();
     });
+  }
 });
